6d3cb28e8f41878e274573be4f8b86215d29f7f4
Tyler Zeman <tylerzeman1999@gmai9l.com>
2022-08-23 18:41:18 -0500

    finished up webpack and db code

--- client/src/js/database.js
+++ client/src/js/database.js
@@ -1,21 +1,41 @@
-import { openDB } from 'idb';
+import { openDB } from "idb";
 
 const initdb = async () =>
-  openDB('jate', 1, {
+  openDB("jate", 1, {
     upgrade(db) {
-      if (db.objectStoreNames.contains('jate')) {
-        console.log('jate database already exists');
+      if (db.objectStoreNames.contains("jate")) {
+        console.log("jate database already exists");
         return;
       }
-      db.createObjectStore('jate', { keyPath: 'id', autoIncrement: true });
-      console.log('jate database created');
+      db.createObjectStore("jate", { keyPath: "id", autoIncrement: true });
+      console.log("jate database created");
     },
   });
 
 // TODO: Add logic to a method that accepts some content and adds it to the database
-export const putDb = async (content) => console.error('putDb not implemented');
-
+export const putDb = async (content) => {
+  console.log("putDB was implemented");
+  const jateDB = await openDB("jate", 1);
+  const transaction = jateDB.transaction("jate", "readwrite");
+  const store = transaction.objectStore("jate");
+  const request = store.put({ id: 1, value: content });
+  const result = await request;
+  console.log("data saved to the database!", result.value);
+};
 // TODO: Add logic for a method that gets all the content from the database
-export const getDb = async () => console.error('getDb not implemented');
-
+export const getDb = async () => {
+  console.log("getDB was implemented");
+  const jateDB = await openDB("jate", 1);
+  const transaction = jateDB.transaction("jate", "readonly");
+  const store = transaction.objectStore("jate");
+  const request = store.get(1);
+  const result = await request;
+  if (result) {
+    console.log("data retrieved from the database", result.value);
+  }
+  if (!result) {
+    console.log("data not found");
+  }
+  return result?.value;
+};
 initdb();
--- client/webpack.config.js
+++ client/webpack.config.js
@@ -4,6 +4,7 @@ const path = require('path');
 const { InjectManifest } = require('workbox-webpack-plugin');
 
 // TODO: Add and configure workbox plugins for a service worker and manifest file.
+
 // TODO: Add CSS loaders and babel to webpack.
 
 module.exports = () => {
@@ -18,12 +19,49 @@ module.exports = () => {
       path: path.resolve(__dirname, 'dist'),
     },
     plugins: [
-      
+      new HtmlWebpackPlugin({
+        template: './index.html',
+        title: 'j.a.t.e'
+      }),
+      new InjectManifest({
+        swSrc: './src-sw.js',
+        swDest: 'src-sw.js'
+      }),
+      new WebpackPwaManifest({
+        fingerprints: false,
+        inject: true,
+        name: 'just another text editor',
+        short_name: 'j.a.t.e',
+        description: 'take notes with javascript syntax highlighting',
+        background_color: '#225ca3',
+        theme_color: '#225ca3',
+        start_url: '/',
+        publicPath: '/',
+        icons: [{
+          src: path.resolve('src/images/logo.png'),
+          sizes: [96, 128, 192, 256, 384, 512],
+          destination: path.join('assets', 'icons'),
+        },
+]
+      })
     ],
 
     module: {
       rules: [
-        
+        {
+          test: /\.css$/i,
+          use: ['style-loader', 'css-loader'],
+        }, {
+          test: /\.m?js$/i,
+          exclude: /node_modules/,
+          use: {
+            loader: 'babel-loader',
+            options: {
+              presets: ['@babel/preset-env'],
+              plugins: ['@babel/plugin-proposal-object-rest-spread', '@babel/transform-runtime'],
+            },
+          }
+        }
       ],
     },
   };
